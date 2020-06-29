899bbf38dc3163ad056c8b158296c28ad2bbf17d
younghak <younghak@nkia.co.kr>
2020-06-29 14:35:14 +0900

    소스정리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
-import Counter from './examples/03_hook/useState/Counter'
+// import Counter from './examples/03_hook/useState/Counter'
+// import Main from './examples/02_hoc/Main'
+import ContainerComponent from './examples/todoAnswer/ContainerComponent'
 
 class App extends Component {
   render () {
     return (
-      <Counter />
+      <ContainerComponent />
+      // <Main />
+      // <Counter />
     )
   }
 }
