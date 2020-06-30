e06e054ed2763a6de1251561c2337591e52f9c39
nyroh <nyroh@nkia.co.kr>
2020-07-01 00:49:13 +0900

    소스 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
 import './App.css'
 import './Container.css';
-// import InputBox from "./todo/01_todoList/01_input/InputBox";
+import InputBox from "./todo/01_todoList/01_input/InputBox";
 // import Counter from './examples/03_hook/useState/Counter'
 // import HocTest from './examples/02_hoc/HocTest'
 // import ContainerComponent from './examples/todoAnswer/ContainerComponent'
-import HookTestPage from './examples/03_hook/HookTestPage'
+// import HookTestPage from './examples/03_hook/HookTestPage'
 class App extends Component {
   render () {
     return (
-        // <InputBox />
+        <InputBox />
         // <MapList/>
         // <Fillter />
         // <ContainerComponent />
       // <div>
-      //   {/* <ContainerComponent /> */}
         // <HocTest />
       //   <Counter />
-      <HookTestPage />
+      //{/*<HookTestPage />*/}
       // </div>
     )
   }
